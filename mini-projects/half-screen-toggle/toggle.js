a15ec59bd49cd0dcc9371af6a62ee30f2b10e3db
Faeze <faezeh.shabannejad@gmail.com>
2025-02-25 14:35:20 +0330

    make the project more challangable
    -most of the changes are if js file
    - I make the project working in this way:
    *while you click on the red button then you click on the blue button , the red button save it style until you click once another. In this way half of the screen is red and half is blue.
    - i use function property and dom in this project
    - its a far different from the previous version (it that one while you click on the red button and then blue, the red button function would be enabled)

--- mini-projects/half-screen-toggle/toggle.js
+++ mini-projects/half-screen-toggle/toggle.js
@@ -1,34 +1,38 @@
+//DOM
+const redButton = document.querySelector(".red-btn");
+const blueButton = document.querySelector(".blue-btn");
+const message = document.querySelector(".message");
+const leftSection = document.querySelector("#left");
+const rightSection = document.querySelector("#right");
+let clickNumberRed = 0;
+let clickNumberBlue = 0;
 // red button
-document.querySelector(".red").addEventListener("click", function () {
-  document.querySelector(".message").textContent = "Your Red!";
-  //the gap between section color and whole body???
-  // document.querySelector(".message").style.backgroundColor = "red";
-  //move the button:when we choose red
-  document.querySelector(".red").style.width = "100%";
-  document.querySelector(".red").style.backgroundColor = "rgba(0, 0, 0, 1)";
-  // when choose red, the blue would be get back to its previous style
-  document.querySelector(".blue").style.width = "10%";
-  document.querySelector(".blue").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-  //left side(red secton) = red
-  document.querySelector("#left").style.backgroundColor = "red";
-  // when we choose blue and then red
-  document.querySelector("#right").style.backgroundColor = "white";
-  //what happend for hovering???
+redButton.addEventListener("click", function () {
+  if (clickNumberRed === 0) {
+    clickNumberRed = 1;
+    redButton.style.width = "100%";
+    redButton.style.backgroundColor = "rgba(0, 0, 0, 1)";
+    leftSection.style.backgroundColor = "red";
+  } else {
+    clickNumberRed = 0;
+    redButton.style.width = "5rem";
+    redButton.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    leftSection.style.backgroundColor = "white";
+  }
 });
+//the gap between section color and whole body???
+
 // blue button
-document.querySelector(".blue").addEventListener("click", function () {
-  document.querySelector(".message").textContent = "Your Blue!";
-  //the gap between section color and whole body???
-  // document.querySelector(".message").style.backgroundColor = "blue";
-  //move the button:when we choose blue
-  document.querySelector(".blue").style.width = "100%";
-  document.querySelector(".blue").style.backgroundColor = "rgba(0, 0, 0, 1)";
-  // when choose blue, the red would be get back to its previous style
-  document.querySelector(".red").style.width = "10%";
-  document.querySelector(".red").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-  //right side(blue section) = blue
-  document.querySelector("#right").style.backgroundColor = "blue";
-  // when we choose red and then blue
-  document.querySelector("#left").style.backgroundColor = "white";
+blueButton.addEventListener("click", function () {
+  if (clickNumberBlue === 0) {
+    clickNumberBlue = 1;
+    blueButton.style.width = "100%";
+    blueButton.style.backgroundColor = "rgba(0, 0, 0, 1)";
+    rightSection.style.backgroundColor = "blue";
+  } else {
+    clickNumberBlue = 0;
+    blueButton.style.width = "5rem";
+    blueButton.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    rightSection.style.backgroundColor = "white";
+  }
 });
-// reset hover effect
