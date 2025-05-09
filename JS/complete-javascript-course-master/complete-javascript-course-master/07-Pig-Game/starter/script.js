19da3d5b16d339c67c1c936f6ac4c6dbe43d5951
Faeze <faezeh.shabannejad@gmail.com>
2025-05-09 21:03:48 +0330

    pig game js
    - defining element by querySelector
    - starting writing functions by clicking btns

--- JS/complete-javascript-course-master/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ JS/complete-javascript-course-master/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+// SELECTIONG ELEMENTS
+const score1El = document.querySelector('#sum-of-scores-1');
+const score2El = document.querySelector('#sum-of-scores-2');
+const currentScore1El = document.querySelector('#current-scores-1');
+const currentScore2El = document.getElementById('current-scores-2');
+const diceEl = document.querySelector('.dice-img');
+const btnNewGame = document.querySelector('.btn-new-game');
+const btnRoll = document.querySelector('.btn-roll-dice');
+const btnHold = document.querySelector('.btn-hold');
+// STARTING CONDITIONS
+score1El.textContent = 0;
+score2El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// ROLLING DICE FUNTIONALITY
