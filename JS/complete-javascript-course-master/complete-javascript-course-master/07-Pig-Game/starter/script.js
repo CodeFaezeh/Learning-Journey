85d3bf1dd63bd61525cb4075b4c7f8d25e56d743
Faeze <faezeh.shabannejad@gmail.com>
2025-05-10 12:11:40 +0330

    pig game : face to bug
    - active player numetricaly is not working and the current  score is not switching after facing to dice number 1

--- JS/complete-javascript-course-master/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ JS/complete-javascript-course-master/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -8,9 +8,53 @@ const diceEl = document.querySelector('.dice-img');
 const btnNewGame = document.querySelector('.btn-new-game');
 const btnRoll = document.querySelector('.btn-roll-dice');
 const btnHold = document.querySelector('.btn-hold');
+/***********************/
+// MY code for distingushing the player
+const activePlayerEl = document.querySelector('.player-active'); // FIXME: the active playet in funtion is not working but ui is work
+const player1El = document.querySelector('.player-1');
+const player2El = document.querySelector('.player-2');
+/***********************/
+
 // STARTING CONDITIONS
 score1El.textContent = 0;
 score2El.textContent = 0;
 diceEl.classList.add('hidden');
+let currentScorePlayer1 = 0;
+let currentScorePlayer2 = 0;
 
 // ROLLING DICE FUNTIONALITY
+btnRoll.addEventListener('click', function () {
+  //1. Roll the dice
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+  console.log(diceNumber);
+  // 2.display the dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${diceNumber}.png`;
+  // 3.0. Check the player
+  if (activePlayerEl.classList.contains('player-1')) {
+    // 3. Check for roll
+    // if dice = 1 what will happen?(switch) else
+    if (diceNumber !== 1) {
+      currentScorePlayer1 += diceNumber;
+      currentScore1El.textContent = currentScorePlayer1;
+      // 3.1.0. Switch the player
+    } else {
+      // 3.1.1. player 1 score would be completely set to zero
+      currentScorePlayer1 = 0;
+      currentScore1El.textContent = currentScorePlayer1;
+      // 3.2. then switching the active player
+      player1El.classList.remove('player-active');
+      player2El.classList.add('player-active');
+    }
+    // player 2 is active
+  } else {
+    if (diceNumber !== 1) {
+      currentScorePlayer2 += diceNumber;
+      currentScore2El.textContent = currentScorePlayer2;
+    } else {
+      currentScorePlayer2 = 0;
+      player2El.classList.remove('player-active');
+      player1El.classList.add('player-active');
+    }
+  }
+});
