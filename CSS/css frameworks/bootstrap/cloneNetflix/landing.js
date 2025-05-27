bcf37bda1cdaf8d21b2ebabc95b55cfea525b7b8
Faeze <faezeh.shabannejad@gmail.com>
2025-05-27 23:38:13 +0330

    update landing page

--- CSS/css frameworks/bootstrap/cloneNetflix/landing.js
+++ CSS/css frameworks/bootstrap/cloneNetflix/landing.js
@@ -1,11 +1,43 @@
 "use strict";
 const navEl = document.querySelector("nav");
+const navLinks = document.querySelectorAll(".nav-link");
+const searchBtnEl = document.querySelector(".btn-search");
 window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
-    navEl.classList.add("bg-dark");
+    navEl.classList.add("bg-custom-menu");
     navEl.classList.remove("bg-transparent");
+
+    navLinks.forEach((link) => (link.style.color = "#fff"));
+    searchBtnEl.classList.remove("btn-outline-dark");
+    searchBtnEl.classList.add("btn-outline-light");
   } else {
-    navEl.classList.remove("bg-light");
+    navEl.classList.remove("bg-custom-menu");
     navEl.classList.add("bg-transparent");
+
+    navLinks.forEach((link) => (link.style.color = "#000"));
+    searchBtnEl.classList.add("btn-outline-dark");
+    searchBtnEl.classList.remove("btn-outline-light");
   }
 });
+// "use strict";
+// const navEl = document.querySelector("nav");
+// const navLinks = document.querySelectorAll(".nav-link");
+// const searchBtnEl = document.querySelector(".btn-search");
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 50) {
+//     navEl.classList.add("bg-custom-menu");
+//     navEl.classList.remove("bg-transparent");
+
+//     navLinks.forEach(link => (link.style.color = "#fff"));
+//     searchBtnEl.classList.remove("btn-outline-dark");
+//     searchBtnEl.classList.add("btn-outline-light");
+//   } else {
+//     navEl.classList.remove("bg-custom-menu");
+//     navEl.classList.add("bg-transparent");
+
+//     navLinks.forEach(link => (link.style.color = "#333")); // or initial
+//     searchBtnEl.classList.remove("btn-outline-light");
+//     searchBtnEl.classList.add("btn-outline-dark");
+//   }
+// });
