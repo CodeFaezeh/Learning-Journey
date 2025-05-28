cf46a4ffe51b390efa09a8702e5bbefa4e7e79de
Faeze <faezeh.shabannejad@gmail.com>
2025-05-28 17:28:59 +0330

    update landing
    - enhance navbar
    - more enteractive (by using js)

--- CSS/css frameworks/bootstrap/cloneNetflix/landing.js
+++ CSS/css frameworks/bootstrap/cloneNetflix/landing.js
@@ -1,43 +1,63 @@
 "use strict";
 const navEl = document.querySelector("nav");
 const navLinks = document.querySelectorAll(".nav-link");
-const searchBtnEl = document.querySelector(".btn-search");
+const dropdownToggle = document.querySelector(".dropdown-toggle");
+const navIconsBtn = document.querySelectorAll(".btn-icon");
+/*************/
+/*NAVBAR */
+/*Navbar Scrolling*/
 window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
     navEl.classList.add("bg-custom-menu");
     navEl.classList.remove("bg-transparent");
 
-    navLinks.forEach((link) => (link.style.color = "#fff"));
-    searchBtnEl.classList.remove("btn-outline-dark");
-    searchBtnEl.classList.add("btn-outline-light");
+    navLinks.forEach((link) => {
+      if (!link.classList.contains("active")) {
+        link.classList.remove("text-dark-custom");
+        link.classList.add("text-light-custom");
+      }
+    });
+
+    dropdownToggle.classList.remove("text-grey-custom");
+    dropdownToggle.classList.add("text-light-custom");
+
+    navIconsBtn.forEach((icon) => {
+      icon.classList.remove("btn-dark-color");
+      icon.classList.add("btn-light-color");
+    });
   } else {
     navEl.classList.remove("bg-custom-menu");
     navEl.classList.add("bg-transparent");
 
-    navLinks.forEach((link) => (link.style.color = "#000"));
-    searchBtnEl.classList.add("btn-outline-dark");
-    searchBtnEl.classList.remove("btn-outline-light");
+    navLinks.forEach((link) => {
+      if (!link.classList.contains("active")) {
+        link.classList.remove("text-light-custom");
+        link.classList.add("text-dark-custom");
+      }
+    });
+
+    dropdownToggle.classList.remove("text-light-custom");
+    dropdownToggle.classList.add("text-grey-custom");
+
+    navIconsBtn.forEach((icon) => {
+      icon.classList.remove("btn-light-color");
+      icon.classList.add("btn-dark-color");
+    });
   }
 });
-// "use strict";
-// const navEl = document.querySelector("nav");
-// const navLinks = document.querySelectorAll(".nav-link");
-// const searchBtnEl = document.querySelector(".btn-search");
-
-// window.addEventListener("scroll", () => {
-//   if (window.scrollY > 50) {
-//     navEl.classList.add("bg-custom-menu");
-//     navEl.classList.remove("bg-transparent");
-
-//     navLinks.forEach(link => (link.style.color = "#fff"));
-//     searchBtnEl.classList.remove("btn-outline-dark");
-//     searchBtnEl.classList.add("btn-outline-light");
-//   } else {
-//     navEl.classList.remove("bg-custom-menu");
-//     navEl.classList.add("bg-transparent");
-
-//     navLinks.forEach(link => (link.style.color = "#333")); // or initial
-//     searchBtnEl.classList.remove("btn-outline-light");
-//     searchBtnEl.classList.add("btn-outline-dark");
-//   }
+/*Navbar select the active link */
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((nav) => {
+      nav.classList.remove("active", "fw-1");
+    });
+    link.classList.add("active");
+  });
+});
+/*FINISH ::: Navbar select the active link */
+// navIconsBtn.forEach((icon) => {
+//   icon.addEventListener("click", () => {
+//     icon.classList.add("btn-active");
+//   });
 // });
+/*************/
