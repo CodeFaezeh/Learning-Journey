24459bef8e17bba0ba92b51784c2ca216376cd89
Faeze <faezeh.shabannejad@gmail.com>
2025-06-17 12:29:33 +0330

    solving leetcode problems
    - problem 2 : adding numbers with carry
    + have bug: runtime error

--- other-projects/algorithmics qs/bootcamp invitation/Q1.js
+++ other-projects/algorithmics qs/bootcamp invitation/Q1.js
@@ -6,6 +6,8 @@
 // // detecSubString("abcabcbb");
 // detecSubString("aBciiiDef");
 
+////Q1:easy
+/*
 const findMaxAdjacentDifference = function (nums) {
   if (nums.length === 0) {
     console.error("Input array is empty.");
@@ -34,3 +36,123 @@ findMaxAdjacentDifference([-5, -10, -5]);
 // maxAdjacentDistance([-10, -5]);
 // maxAdjacentDistance([-5, -5]);
 // maxAdjacentDistance([-5, -10]);
+*/
+////Q2:easy
+/*
+let isPalindrome = function (num) {
+  num = String(num);
+  const n = num.length;
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    if (num[i] === num[n - (i + 1)]) {
+    } else {
+      return false;
+    }
+  }
+  console.log(true);
+  return true;
+};
+isPalindrome(1);
+// isPalindrome(32);
+// isPalindrome(345);
+// isPalindrome(1001);
+// isPalindrome(1021);
+*/
+////Q3:easy
+/*
+function romanToInt(r) {
+  const roman = new Map([
+    ["I", 1],
+    ["V", 5],
+    ["X", 10],
+    ["L", 50],
+    ["C", 100],
+    ["D", 500],
+    ["M", 1000],
+  ]);
+  r = r.split("");
+  let result = 0;
+  for (let i = 0; i < r.length; i++) {
+    const currentValue = roman.get(r[i]);
+    const nextValue = roman.get(r[i + 1]);
+    if (currentValue < nextValue) {
+      result -= currentValue;
+    } else {
+      result += currentValue;
+    }
+  }
+  console.log(result);
+  return result;
+}
+// romanToInt("MC");
+romanToInt("MCMXCIV");
+// romanToInt("XV");
+// romanToInt("III");
+// romanToInt("LVIII");
+// romanToInt("DM");
+*/
+////Q4:easy-remove duplicates
+/*
+function removeDuplicates(nums) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  console.log(nums);
+  return;
+}
+removeDuplicates(["1", "1", "3"]);
+removeDuplicates([1, 1, 2]);
+*/
+///Q:med , add two numbers
+let addTwoNumbers = function (l1, l2) {
+  // first:equall length of arrays
+  n = Math.max(l1.length, l2.length);
+  let diff1 = n - l1.length;
+  let diff2 = n - l2.length;
+  if (n > l1.length) {
+    for (let i = 0; i < diff1; i++) {
+      l1.push(0);
+    }
+  }
+  if (n > l2.length) {
+    for (let i = 0; i < diff2; i++) {
+      l2.push(0);
+    }
+  }
+  // second: add each number of array to each other
+  let l3 = [];
+  let carry = [0];
+  l1.forEach((digit, index) => {
+    let sum = digit + l2[index] + carry[index];
+    // l3[index]
+    if (sum >= 10) {
+      l3[index] = sum - 10;
+      carry.push(1);
+      if (l3.length === n) {
+        l3.push(1);
+      }
+    } else {
+      l3[index] = sum;
+      carry.push(0);
+    }
+  });
+  // console.log(arr1, arr2);
+  // console.log(l3);
+  // console.log(l3.reverse());
+  console.log(l3.reverse().join(""));
+};
+
+//432,34500 = 34932
+// arr1 = [2, 3, 4];
+// arr2 = [0, 0, 5, 4, 3];
+//5
+//16
+// arr1 = [5];
+// arr2 = [6, 1];
+arr1 = [8];
+arr2 = [2];
+
+addTwoNumbers(arr1, arr2);
