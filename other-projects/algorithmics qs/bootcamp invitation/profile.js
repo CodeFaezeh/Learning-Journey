b04ef118282b583da05a66dafc75216d32945d96
Faeze <faezeh.shabannejad@gmail.com>
2025-07-04 02:10:25 +0330

    v1:update
    comment and fix misleading words

---
+++ other-projects/algorithmics qs/bootcamp invitation/profile.js
@@ -0,0 +1,7 @@
+// Hovering
+const socialMediasIcons = document.querySelectorAll(".socialMedias");
+socialMediasIcons.forEach((icon) => {
+  icon.addEventListener("mouseenter", () => {
+    icon.src;
+  });
+});
