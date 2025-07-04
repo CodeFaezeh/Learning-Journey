21164df2bf49e8d9b8b4b8229cbf84ee1597841d
Faeze <faezeh.shabannejad@gmail.com>
2025-07-04 15:56:05 +0330

    start responsive

--- other-projects/algorithmics qs/bootcamp invitation/profile.js
+++ other-projects/algorithmics qs/bootcamp invitation/profile.js
@@ -1,7 +1,22 @@
 // Hovering
-const socialMediasIcons = document.querySelectorAll(".socialMedias");
+//change social medias icon
+const socialMediasIcons = document.querySelectorAll(".socialMedias img");
 socialMediasIcons.forEach((icon) => {
   icon.addEventListener("mouseenter", () => {
-    icon.src;
+    icon.src = icon.dataset.hover;
+  });
+  icon.addEventListener("mouseleave", () => {
+    icon.src = icon.dataset.original;
+  });
+});
+const dashboardItems = document.querySelectorAll(".dashboard-eachItem");
+dashboardItems.forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    item.style.backgroundColor = "#1DA1F214";
+    item.classList.add("hoverd");
+  });
+  item.addEventListener("mouseleave", () => {
+    item.style.backgroundColor = "transparent";
+    item.classList.remove("hoverd");
   });
 });
